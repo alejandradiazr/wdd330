ab3677d4329b4c879e8de94c1a9bea2853ddc830
alejandradiazr <aleja.diaz.2023@gmail.com>
2026-09-14 19:15:46 -0500

    Fix GitHub Pages paths

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -15,7 +15,7 @@ export default class ProductList {
     productCardTemplate(product) {
         return `<li class="product-card">
             <a href="product_pages/?product=${product.Id}">
-                <img src="${product.Image}" alt="Image of ${product.Name}">
+                <img src="${import.meta.env.BASE_URL}${product.Image.replace(/^\//, '')}" alt="Image of ${product.Name}">
                 <h2 class="card__brand">${product.Brand.Name}</h2>
                 <h3 class="card__name">${product.NameWithoutBrand}</h3>
                 <p class="product-card__price">$${product.FinalPrice}</p>
