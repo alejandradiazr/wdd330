de3045c055fd473fbb97f89281b43e30e2a8291f
alejandradiazr <aleja.diaz.2023@gmail.com>
2026-09-24 16:11:41 -0500

    W03 Individual Activity: Expand Inventory

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -9,13 +9,15 @@ function convertToJson(res) {
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `../json/${this.category}.json`;
+    this.path = `${import.meta.env.VITE_SERVER_URL}products/search/${category}`;
   }
+
   getData() {
     return fetch(this.path)
       .then(convertToJson)
-      .then((data) => data);
+      .then((data) => data.Result);
   }
+
   async findProductById(id) {
     const products = await this.getData();
     return products.find((item) => item.Id === id);
