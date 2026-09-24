23cde7e911979a10ddf08946c46f88d0bde0b2fe
alejandradiazr <aleja.diaz.2023@gmail.com>
2026-09-23 20:09:01 -0500

    W03 Team Activity: Dynamic Header and Footer

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,30 +1,72 @@
 // wrapper for querySelector...returns matching element
+
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
 }
 
 // retrieve data from localstorage
+
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 
 // save data to local storage
+
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
 
 // set a listener for both touchend and click
+
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
     event.preventDefault();
     callback();
   });
+
   qs(selector).addEventListener("click", callback);
 }
 
 // retrieve a parameter from the URL
+
 export function getParam(param) {
   const queryString = window.location.search;
   const params = new URLSearchParams(queryString);
   return params.get(param);
+}
+
+// render a template
+
+export function renderWithTemplate(
+  template,
+  parentElement,
+  data,
+  callback
+) {
+  parentElement.innerHTML = template;
+
+  if (callback) {
+    callback(data);
+  }
+}
+
+// load an HTML template
+
+export async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+// load the header and footer
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("/partials/header.html");
+  const footerTemplate = await loadTemplate("/partials/footer.html");
+
+  const headerElement = document.querySelector("#main-header");
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
 }
