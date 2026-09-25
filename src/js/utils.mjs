c0790a4073caee211a96023cf58dce3eb433f62a
alejandradiazr <aleja.diaz.2023@gmail.com>
2026-09-24 21:36:44 -0500

    Fix template paths for base URL

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,34 +1,28 @@
 // wrapper for querySelector...returns matching element
-
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
 }
 
 // retrieve data from localstorage
-
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 
 // save data to local storage
-
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
 
 // set a listener for both touchend and click
-
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
     event.preventDefault();
     callback();
   });
-
   qs(selector).addEventListener("click", callback);
 }
 
 // retrieve a parameter from the URL
-
 export function getParam(param) {
   const queryString = window.location.search;
   const params = new URLSearchParams(queryString);
@@ -36,22 +30,14 @@ export function getParam(param) {
 }
 
 // render a template
-
-export function renderWithTemplate(
-  template,
-  parentElement,
-  data,
-  callback
-) {
+export function renderWithTemplate(template, parentElement, data, callback) {
   parentElement.innerHTML = template;
-
   if (callback) {
     callback(data);
   }
 }
 
 // load an HTML template
-
 export async function loadTemplate(path) {
   const res = await fetch(path);
   const template = await res.text();
@@ -59,27 +45,32 @@ export async function loadTemplate(path) {
 }
 
 // load the header and footer
-
 export async function loadHeaderFooter() {
-  const headerTemplate = await loadTemplate("/partials/header.html");
-  const footerTemplate = await loadTemplate("/partials/footer.html");
-
+  const headerTemplate = await loadTemplate(
+    `${import.meta.env.BASE_URL}partials/header.html`
+  );
+  const footerTemplate = await loadTemplate(
+    `${import.meta.env.BASE_URL}partials/footer.html`
+  );
   const headerElement = document.querySelector("#main-header");
   const footerElement = document.querySelector("#main-footer");
 
   renderWithTemplate(headerTemplate, headerElement);
   renderWithTemplate(footerTemplate, footerElement);
-  export function renderListWithTemplate(
-    templateFn,
-    parentElement,
-    list,
-    position = 'afterbegin',
-    clear = false
-  ) {
-    if (clear) {
-      parentElement.innerHTML = '';
-    }
+}
 
-    const htmlStrings = list.map(templateFn);
-    parentElement.insertAdjacentHTML(position, htmlStrings.join(''));
+// render a list using a template function
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  if (clear) {
+    parentElement.innerHTML = "";
   }
+
+  const htmlStrings = list.map(templateFn);
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
